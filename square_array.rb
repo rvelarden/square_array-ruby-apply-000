4a62aaf0abc03a5fd7eaab438995b76d70fef6ed
Romina Velarde <rominapaola.velarde@gmail.com>
2020-05-26 22:02:02 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
 def square_array(array)
-  # your code here
+square_numbers = []
+
+array.each do |number|
+# square_numbers.push(number * number)
+square_numbers<< number ** 2
 end
+square_numbers
+end
+# def square_array(array)
+
+
+# array.collect do |number|
+# # square_numbers.push(number * number)
+# number ** 2
+# end
+# end
+ 
